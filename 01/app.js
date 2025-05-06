b3ac9f3a38fb9f19e6b7449d5ef2a13587f557d1
Daria Seńko <twoj@email>
2025-05-06 19:59:10 +0200

    01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const UlElement = document.querySelector('ul');
+if (UlElement) {
+  const classElement = UlElement.querySelector(
+    '.comments__item.comments__item--newest'
+  );
+  const dataInfo = classElement.querySelectorAll('[data-info]');
+  console.log(dataInfo.length);
+}
