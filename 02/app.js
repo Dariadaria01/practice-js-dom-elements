d0aa55d5dd0d4c6bb1820abc1b63a7b9f1ed3a1b
Daria Seńko <twoj@email>
2025-05-06 20:27:02 +0200

    02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,11 @@
 console.log('DOM');
+
+const linksEl = document.querySelectorAll('[data-url]');
+linksEl.forEach((link) => {
+  const url = link.dataset.url;
+  if (url && url !== 'null') {
+    link.setAttribute('href', url);
+  } else {
+    link.setAttribute('href', '#');
+  }
+});
