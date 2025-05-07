8619710028e474ef315abb870c8b15ebaaa00f7d
Daria Seńko <twoj@email>
2025-05-07 18:18:50 +0200

    03

--- 03/app.js
+++ 03/app.js
@@ -1,14 +1,28 @@
 console.log('DOM');
 
 const buttonSettings = {
-    attr: {
-        className: 'btn',
-        title: 'super button'
-    },
-    css: {
-        border: '1px solid #336699',
-        padding: '5px 20px',
-        color: '#444'
-    },
-    text: 'Click me!',
+  attr: {
+    className: 'btn',
+    title: 'super button',
+  },
+  css: {
+    border: '1px solid #336699',
+    padding: '5px 20px',
+    color: '#444',
+  },
+  text: 'Click me!',
+};
+
+const button = document.createElement('button');
+for (const key in buttonSettings.attr) {
+  button[key] = buttonSettings.attr[key];
+}
+for (const styleKey in buttonSettings.css) {
+  button.style[styleKey] = buttonSettings.css[styleKey];
+}
+button.innerText = buttonSettings.text;
+
+const parentElement = document.querySelector('.parent-for-button');
+if (parentElement) {
+  parentElement.appendChild(button);
 }
