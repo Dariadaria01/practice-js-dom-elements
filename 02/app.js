19a926323dbda4d61fba34edbfaec6d47d02ff19
Daria Seńko <twoj@email>
2025-05-21 16:28:58 +0200

    02

--- 02/app.js
+++ 02/app.js
@@ -3,9 +3,5 @@ console.log('DOM');
 const linksEl = document.querySelectorAll('[data-url]');
 linksEl.forEach((link) => {
   const url = link.dataset.url;
-  if (url && url !== 'null') {
-    link.setAttribute('href', url);
-  } else {
-    link.setAttribute('href', '#');
-  }
+  link.setAttribute('href', url);
 });
